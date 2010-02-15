50675278ab3ea3b558604b663aa43dd4b7a58934
benjones <ben.james.jones@gmail.com>
2010-02-14 19:16:13 -0800

    rudimentary parsing

---
+++ src/radianceSplat.cpp
@@ -0,0 +1,14 @@
+#include "SceneObject.h"
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+  
+  std::ifstream ins;
+  ins.open(argv[1]);
+  SceneObject p(ins);
+  
+
+  return 0;
+}
